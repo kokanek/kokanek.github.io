40f7712f62dabb389b38d4f8f34f775c3f3eb866
kokanek <kapeel.kokane@gmail.com>
2020-04-11 08:48:52 +0530

    added the youtube panel in the profile section

--- assets/js/main.js
+++ assets/js/main.js
@@ -53,7 +53,5 @@ jQuery(document).ready(function($) {
     );
     
     /* Github Activity Feed - https://github.com/caseyscarborough/github-activity */
-    GitHubActivity.feed({ username: "caseyscarborough", selector: "#ghfeed" });
-
 
 });
